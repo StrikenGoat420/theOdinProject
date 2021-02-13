718f63456e80954fe09ae7bb09024e1c6db235a6
Shubham <shubhampareek4000@gmail.com>
2021-02-14 02:05:02 +0530

    implemented add and remove book feature

--- library/js/script.js
+++ library/js/script.js
@@ -1,18 +1,10 @@
 /*
 Things done so far :
     Input is taken from form already, and new book is added to the library
+    Add and Remove from library is done
 
 TODO:
-    Show all books in library on the grid
-
-    When a new book is added, add it before the new book div
-
-    Show option to remove book from library on UI
-
-    //something like this
-    removeButton.addEventListener('click', () => {
-        library.removeBook(removeButton.id) 
-    })
+    When user clicks on the book, show pop up screen with all the info about the book and let user change read status
 
     Add sorting functionality
 
@@ -49,21 +41,43 @@ class Library {
         this.id += 1;      
     }
 
-    removeBook(id){
-        this.shelf.splice(id, 1); //removing book with particular id
+    removeBook(idNum){
+        if (idNum > this.id){
+            console.log("Element doesnt exist in Library");
+            return "Element doesnt exist in Library";
+        }
+        this.shelf.splice(idNum, 1); //removing book with particular id
         this.bookCount = this.shelf.length; //updating bookCount
         //changing id of every book after id = N, where N is the id of the book that was deleted
-        for (let i = id; i<this.bookCount; i++){
+        for (let i = idNum; i<this.bookCount; i++){
             this.shelf[i].id = i;
         }
         //updating id, so that we do not end up skipping an id
         this.id -= 1;
     }
 
-    showBookDetails () {
-        for (let book of this.shelf){
-            console.log(book.id, book.name);
+    showBookDetails (idNum) {
+        //func takes in an id number and returns and array of the book details
+
+        //todo : find better way to loop over all properties of the book class
+        let bookTBDisplay = this.shelf[idNum];
+        let bookDetails = {
+            name : bookTBDisplay.name,
+            author : bookTBDisplay.author,
+            page : bookTBDisplay.pages,
+            read : bookTBDisplay.read,
         }
+        return bookDetails;
+    }
+
+    getShelf(){
+        return this.shelf;
+    }
+
+    getLastBook (){
+        //function that returns that last book in the shelf
+        //since id is created in the library class, to get access to the id we need this to get the id of the last added book
+        return this.shelf[this.shelf.length - 1];
     }
 }
 function showForm(addButton){
@@ -78,7 +92,40 @@ function closePopup() {
     popup.classList.remove("popupContainerActivated");
     overlay.classList.remove("overlay--active");
 }
-   
+
+function getBookContainer(id, name){
+    let bookContainer = document.createElement('div');
+    bookContainer.className = 'bookContainer';
+    let bookId = 'b'+id;
+    bookContainer.id = bookId;
+
+    let deleteBookContainer = document.createElement('div');
+    deleteBookContainer.className = 'deleteBookContainer';
+
+    let infoText = document.createElement('h3');
+    infoText.textContent = 'Click Book for Info';
+    infoText.className = 'showBookInfo';
+    deleteBookContainer.appendChild(infoText);
+
+    let deleteButton = document.createElement('button');
+    let delId = 'd'+id;
+    deleteButton.className = 'deleteBookButton';
+    deleteButton.id = delId;
+    deleteButton.type = 'close';
+    deleteButton.innerText = 'Del';
+    deleteBook(deleteButton) //adding delete button functionality over here itself
+    
+    deleteBookContainer.appendChild(deleteButton);
+    bookContainer.appendChild(deleteBookContainer);
+
+    let book = document.createElement('div');
+    book.className = 'book'
+    book.innerText = name;
+    
+    bookContainer.appendChild(book);
+    return bookContainer;
+}
+
 function createBook (event){
     event.preventDefault();
     //console.log(event)
@@ -101,10 +148,73 @@ function createBook (event){
             bookRead = true;
         }
     }
+    closePopup();
 
     library.addBook(bookName, authorName, pageNumber, bookRead);
-    library.showBookDetails()
-    closePopup();
+    // library.showBookDetails()
+    let book = library.getLastBook(); //the book that was just added in the library. Will be used to get the id of the book.
+
+    let bookContainer = getBookContainer(book.id, bookName);
+
+    let addBookContainer = document.querySelector('.addNewBookContainer');
+    const shelf = document.querySelector('.shelf');
+    shelf.insertBefore(bookContainer, addBookContainer);    
+
+}
+
+function deleteBook(button){
+    button.addEventListener('click', () => {
+        let bookId = button.id;
+        bookId = Number(bookId.substring(1,)) //we need the number after the first character
+        library.removeBook(bookId);
+        // let shelf = library.getShelf(); //contains updated list all the book in the library
+        let bookDivID = 'b'+bookId;
+        //console.log(bookDivID)
+        let elToBeDeleted = document.getElementById(bookDivID);
+        bookShelf.removeChild(elToBeDeleted);
+
+        //console.log(`bookId is ${bookId} and lib id is ${library.id}`);
+        for (let i = bookId+1; i<library.id ; i++){
+            let newIdNum = i-1;
+            let bookIdToBeChanged = document.getElementById('b'+i);
+            let delIdToBeChanged = document.getElementById('d'+i);
+            bookIdToBeChanged.id = 'b'+newIdNum;
+            delIdToBeChanged.id = 'd'+newIdNum;
+
+        }
+    })
+}
+
+function showBookProp(button){
+    button.addEventListener('hover',() => {
+        //do something, see if we can get the pressing effect
+        let aaaaaaaaaa = 11;
+    })
+    button.addEventListener('click', () => {
+        let bookId = button.parentNode.id;
+        bookId = Number(bookId.substring(1,));
+        let bookInfo = library.showBookDetails(bookId)
+        console.log(bookInfo);
+        /*
+            Continue from here
+            We have the book info now of the given ID. Use it show 
+            the book details in the popup
+
+            Do similarly to how we did the popup form
+                -> write html and css for the pop up and before and after class
+                -> fill details in it using the bookInfo we have
+                -> for Read do something like (design):
+                    Read : true  changeStatus
+
+                    where changeStatus is the button which will change the status of the read field
+                    if button is pressed {
+                        library.shelf[i].read = !library.shelf[i].read
+                    }
+                    essentially read = !read  
+
+        */
+
+    })
 }
 
 function addButtonFunctionality(){
@@ -117,6 +227,7 @@ function addButtonFunctionality(){
     });
 
     form.addEventListener("submit", createBook); //if user presses the submit button
+
     /*
     functionality which lets user press the enter button to submit the form. TODO
     window.addEventListener("keydown", (event) => {
@@ -125,16 +236,32 @@ function addButtonFunctionality(){
         } 
     });
     */
+
+    deleteButton.forEach(deleteBook) //this is just as to showcase an example, can remove if we remove all the pre inserted books
+    bookDivs.forEach(showBookProp);
 }
 
 const library = new Library();
+library.addBook(1,2,3,true);
+library.addBook(4,5,6,true);
+library.addBook(7,8,9,true);
+library.addBook(10,11,12,false);
+library.addBook(13,14,15,false);
 
+const bookShelf = document.querySelector('.shelf')
 const addButtons = document.querySelectorAll('.addBooksButton');
 const popup = document.querySelector('.popupContainer');
 const overlay = document.querySelector(".js-overlay");
 const form = document.querySelector(".bookInfoForm");
 const formSubmitButton = document.querySelector('#submitButton');
 
+const deleteButton = document.querySelectorAll('.deleteBookButton');
+const bookDivs = document.querySelectorAll('.book')
+
+
+//console.log(delButton.parentNode.parentNode.id) //could have used this to get id of book, but instead will just assign each del its own id
+
+//bookShelf.removeChild(document.getElementById('b200'))
 addButtonFunctionality();
 
 
